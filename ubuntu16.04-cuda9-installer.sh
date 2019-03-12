75cb90ec4f3b2bd4ef5c55fcae8a965679858b96
Luke Barr <lukebarr@outlook.com>
2019-03-12 04:07:28 -0500

    Add Ubuntu 16.04 Install Script

---
+++ ubuntu16.04-cuda9-installer.sh
@@ -0,0 +1,37 @@
+# Ubuntu 16.04 Installer
+
+# Add NVIDIA package repository
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+sudo apt -y install ./cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
+sudo apt -y install ./nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
+sudo apt update
+
+sudo mkdir /usr/lib/nvidia
+sudo apt -y install nvidia-418
+
+sudo apt -y install cuda-9-0 cuda-cublas-9-0 cuda-cufft-9-0 cuda-curand-9-0 \
+    cuda-cusolver-9-0 cuda-cusparse-9-0 libcudnn7=7.3.1.20-1+cuda9.0 \
+    libcudnn7-dev=7.3.1.20-1+cuda9.0 libnccl2=2.2.13-1+cuda9.0 cuda-command-line-tools-9-0 
+
+sudo apt -y install cuda-toolkit-9-0
+
+echo "export PATH=/usr/local/cuda-9.0/bin:$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+source ~/.bashrc
+
+# Install Python3.6
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6 python3.6-dev
+
+# Install DeepFaceLab Dependencies
+sudo apt -y install ffmpeg cmake build-essential git
+
+# Install DeepFaceLab Python Dependencies
+sudo python3.6 -m pip install -r requirements-cuda.txt
+
+# Test DLIB
+python3.6 -c 'import dlib;print("DLIB CUDA SUPPORT:", dlib.DLIB_USE_CUDA)'
+echo "Please reboot your system to let the new changes take effect."
