77b7b7d0420d5053871d30f20ab942f0b1bfea67
Luke Barr <lukebarr@outlook.com>
2019-03-13 20:12:14 -0500

    Multiprocess fix moved to main.py

--- joblib/SubprocessorBase.py
+++ joblib/SubprocessorBase.py
@@ -135,11 +135,6 @@ class Subprocessor(object):
         pass
  
     def run(self):
-        try:
-            multiprocessing.set_start_method("spawn")
-        except RuntimeError:
-            pass
-
         self.clis = []
             
         #getting info about name of subprocesses, host and client dicts, and spawning them
