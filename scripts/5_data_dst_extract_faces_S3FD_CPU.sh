5642c17a82b955d30877e025d7688fa748f08737
Luke Barr <lukebarr@outlook.com>
2019-03-18 03:12:41 -0500

    Remove CPU Scripts

--- scripts/5_data_dst_extract_faces_S3FD_CPU.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source env.sh
-
-$DFL_PYTHON "$DFL_SRC/main.py" extract \
-    --input-dir "$DFL_WORKSPACE/data_dst" \
-    --output-dir "$DFL_WORKSPACE/data_dst/aligned" \
-    --cpu-only \
-    --detector s3fd
